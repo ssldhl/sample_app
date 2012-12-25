03887b78f11fdab8496288df0667970cf4948e87
sushilthe <sushilthe@gmail.com>
2012-12-25 22:04:49 +0545

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
 	subject { page }
+
 	describe "signup page" do
     before { visit signup_path }
 
@@ -21,23 +22,27 @@ describe "UserPages" do
 
     before { visit signup_path }
 
-      let(:submit) { "Create my account" }
-      describe "with invalid information" do
-        it "should not create a user" do
-          expect { click_button submit}.not_to change(User, :count)
-       end
-     end
+    let(:submit) { "Create my account" }
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit}.not_to change(User, :count)
+      end
+    end
 
-      describe "with valid information" do
-       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
+    describe "with valid information" do
+      before do
+        fill_in "User Name",         with: "Example User"
+        fill_in "E-mail",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+          fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        it { should have_link('Sign out') }
       end
     end
   end
