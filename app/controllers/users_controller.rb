3de25d545a8c0b6e5677277ed731f908cd2e72ad
sushilthe <sushilthe@gmail.com>
2012-12-27 22:50:14 +0545

    Restrict users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :show]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
   before_filter :sign_in_create, only: [:new, :create]
